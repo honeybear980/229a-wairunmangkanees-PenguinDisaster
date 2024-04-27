40b43e6076712f5204afbc8a43df287f8d2b432e
honeybear980 <lappland980@gmail.com>
2024-04-27 19:33:14 +0700

    Jump

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] int jumpPower;
+    private Rigidbody2D body;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed ,body.velocity.y);
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpPower);
+        isGrounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+}
